2a8041b7a524829e676c3bab9eacd851304f529c
Ahmed Muftić <50627622+ahmedmufta98@users.noreply.github.com>
2021-04-24 19:38:35 +0200

    Docker

---
+++ eHairdresserSalonFare/eHairdresserSalonFare.Api/Helper/DbSeed.cs
@@ -0,0 +1,20 @@
+﻿using eHairdresserSalonFareBugojno.DBContext;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace eHairdresserSalonFare.Api.Helper
+{
+    public static class DbSeed
+    {
+        public static void SeedDB(AppDbContext context)
+        {
+            try
+            {
+                context.Database.Migrate();
+            }catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ eHairdresserSalonFare/eHairdresserSalonFare.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using eHairdresserSalonFare.Api.Helper;
+using eHairdresserSalonFareBugojno.DBContext;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eHairdresserSalonFare.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = CreateWebHostBuilder(args).Build();
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                AppDbContext context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                DbSeed.SeedDB(context);
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
